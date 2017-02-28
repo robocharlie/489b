55700fb206c91cb9c3e13349aab881525466eaad
Nathan <njyoung94@gmail.com>
2017-02-28 10:00:16 -0500

    pywinauto test

--- lab2/shift.py
+++ lab2/shift.py
@@ -15,7 +15,7 @@ GPIO.setup(latch_pin, GPIO.OUT, initial=0)
 GPIO.setup(clock_pin, GPIO.OUT, initial=0)
 
 #four = int('00110011', 2)
-four = int('11111111', 2)
+four = int('1000000', 2)
 mask = 1
 
 for i in range(8):
