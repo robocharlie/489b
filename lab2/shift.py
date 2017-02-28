a5164b154243de90537368ab14286f6fbf195fc4
Nathan <njyoung94@gmail.com>
2017-02-28 09:49:47 -0500

    pywinauto test

--- lab2/shift.py
+++ lab2/shift.py
@@ -1,13 +1,12 @@
 # dependencies
-#import RPi.GPIO as GPIO
-import fakeRPiGPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 # Port numbering
 GPIO.setmode(GPIO.bcm)
 
 # define pins
-data_pin = 6
+data_pin = 16
 latch_pin = 12
 clock_pin = 6
 
@@ -20,8 +19,15 @@ four = int('00110011', 2)
 mask = 1
 
 for i in range(8):
+    # if we want the led off, set gpio high
     if four & mask == 0:
         GPIO.OUTPUT(data_pin, 1)
+    # else we want it on, set gpio low
     else:
         GPIO.OUTPUT(data_pin, 0)
     mask <<= 1
+
+    GPIO.OUTPUT(clock_pin, 1)
+    time.sleep(0)
+
+    GPIO.OUTPUT(clock_pin, 0)
