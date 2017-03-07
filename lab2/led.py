01f7ad2c7517113f775365fbf99341a7af544bcf
Nathan <njyoung94@gmail.com>
2017-03-06 21:20:00 -0500

    preliinary lab 2 stuff

--- lab2/led.py
+++ lab2/led.py
@@ -7,15 +7,18 @@ import time
 the_LEDdisplay = LEDdisplay(16, 12, 6)
 
 # Spam random combinations
-for val in range(100):
+for i in range(100):
     the_LEDdisplay.set_random_pins()
-    print(val)
     time.sleep(.01)
 
 the_LEDdisplay.patterns('spiral', False, .2, 5)
 the_LEDdisplay.patterns('circle', False, .2, 3)
 the_LEDdisplay.patterns('spiral', True, .2, 5)
 the_LEDdisplay.patterns('circle', True, .2, 3)
+
+for i in range(len(the_LEDdisplay.numbers)):
+    the_LEDdisplay.set_number(i)
+    time.sleep(1/i)
 
 # cleanup
 GPIO.cleanup()
