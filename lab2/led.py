a1a348eec565d1776e56f53d05937474577deb6f
Nathan <njyoung94@gmail.com>
2017-03-06 21:23:04 -0500

    preliinary lab 2 stuff

--- lab2/led.py
+++ lab2/led.py
@@ -11,10 +11,10 @@ for i in range(100):
     the_LEDdisplay.set_random_pins()
     time.sleep(.01)
 
-the_LEDdisplay.patterns('spiral', False, .2, 5)
-the_LEDdisplay.patterns('circle', False, .2, 3)
-the_LEDdisplay.patterns('spiral', True, .2, 5)
-the_LEDdisplay.patterns('circle', True, .2, 3)
+the_LEDdisplay.patterns('spiral', False, .2, 1)
+the_LEDdisplay.patterns('circle', False, .2, 1)
+the_LEDdisplay.patterns('spiral', True, .2, 1)
+the_LEDdisplay.patterns('circle', True, .2, 1)
 
 for i in range(len(the_LEDdisplay.numbers)):
     the_LEDdisplay.set_number(i)
