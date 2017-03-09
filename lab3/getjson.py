b698b0405ca6ff030c3b733be4d2e85da0a4bab1
Nathan <njyoung94@gmail.com>
2017-03-09 09:42:25 -0500

    fixed getjson

--- lab3/getjson.py
+++ lab3/getjson.py
@@ -2,10 +2,10 @@ import urllib2
 import json
 
 
-def getJSONdata(urlData):
-    webUrl = urllib2.urlopen(urlData) # Open the URL and read the data
-    if (webUrl.getcode() == 200): # verify that "ok" code received
+def getJSON(urlData):
+    webUrl = urllib2.urlopen(urlData)  # Open the URL and read the data
+    if webUrl.getcode() == 200:  # verify that "ok" code received
         data = webUrl.read()
     else:
         data = "Server error: " + str(webUrl.getcode())
-    return(json.loads(data))
+    return json.loads(data)
