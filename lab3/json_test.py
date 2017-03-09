418dedd74252be9654126566c37eb557ec39dfea
Nathan <njyoung94@gmail.com>
2017-03-09 09:55:00 -0500

    new url

--- lab3/json_test.py
+++ lab3/json_test.py
@@ -1,7 +1,8 @@
 from getjson import getJSON
 
 
-theURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+#theURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+theURL = "http://data.my.gov/api/views/d6yy-54nr/rows.json"
 theJSON = getJSON(theURL)
 
 print(theJSON)
