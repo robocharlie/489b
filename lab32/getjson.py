5c6a27d324c4d24b786594e18eea28919dc5e872
Nathan <njyoung94@gmail.com>
2017-03-16 09:59:55 -0400

    added and fixed lab32 (in class stuff)

--- lab32/getjson.py
+++ lab32/getjson.py
@@ -2,10 +2,11 @@
 import urllib2
 import json
 
+
 def getJSON(urlData):
-  webUrl = urllib2.urlopen(urlData) 	# Open the URL and read the data
-  if (webUrl.getcode() == 200):		# verify that "ok" code received
-    data = webUrl.read()
-  else:
-    data = "Server error: " + str(webUrl.getcode())
-  return(json.loads(data))
+    webUrl = urllib2.urlopen(urlData) 	# Open the URL and read the data
+    if webUrl.getcode() == 200:		# verify that "ok" code received
+        data = webUrl.read()
+    else:
+        data = "Server error: " + str(webUrl.getcode())
+    return json.loads(data)
--- lab32/lcd_INCLASS.py
+++ lab32/lcd_INCLASS.py
@@ -7,70 +7,80 @@
 
 import RPi.GPIO as gpio
 import time
-from subprocess import *	# need for getIP() function
+from subprocess import *  # need for getIP() function
 from shifter import Shifter
 from getjson import getJSON
 
-gpio.setmode(gpio.BCM)	# use GPIO pin numbering for extension board
+gpio.setmode(gpio.BCM)  # use GPIO pin numbering for extension board
 
 # Pin configuration:
 rs = 26  # register select (LCD pin 4)
 en = 13  # clock enable (LCD pin 6)
 for pin in (rs, en):
-  gpio.setup(pin, gpio.OUT, initial=0)
+    gpio.setup(pin, gpio.OUT, initial=0)
 
 dataPin, latchPin, clockPin = 16, 12, 6
 theShifter = Shifter(dataPin, latchPin, clockPin)   # create shift register object
 
-def getIP():
-  cmd = "ip addr show wlan0 | grep 'inet\ ' | awk '{print $2}' | cut -d/ -f1"
-  p = Popen(cmd, shell=True, stdout=PIPE)
-  output = p.communicate()[0]
-  output = output.strip()    # remove whitespace characters
-  return output
 
-def delay_us(microsec):   
-  endTime = time.time() + (float(microsec)/1000000.0)
-  while time.time() < endTime:
-    pass
+def getIP():
+    cmd = "ip addr show wlan0 | grep 'inet\ ' | awk '{print $2}' | cut -d/ -f1"
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    output = output.strip()    # remove whitespace characters
+    return output
+
+
+def delay_us(microsec):
+    endTime = time.time() + (float(microsec)/1000000.0)
+    while time.time() < endTime:
+        pass
+
 
 def ping():   # ping the clock enable pin (falling edge)
-  gpio.output(en, 1)
-  delay_us(100)
-  gpio.output(en, 0)
-  delay_us(1)
+    gpio.output(en, 1)
+    delay_us(100)
+    gpio.output(en, 0)
+    delay_us(1)
+
 
 def write(value, inputMode = 0):
-  delay_us(1000)	# 1 ms delay to be safe
-  gpio.output(rs, inputMode)    # set input mode (0=instruction, 1=character)
-  theShifter.setValue(value)
-  ping()
+    delay_us(1000)  # 1 ms delay to be safe
+    gpio.output(rs, inputMode)    # set input mode (0=instruction, 1=character)
+    theShifter.setValue(value)
+    ping()
+
 
 def set_row_col(row, col=0):  # set the cursor row + column position
-  ddram = int('10000000', 2)	# base instruction for setting DRAM address 
-  # set display data RAM (DDRAM) address for row/col
-  # (see pg. 196 of datasheet, noting 0x40 = b1000000 = 1 << 6)
-  write(ddram | ((row << 6) + col))	
+    ddram = int('10000000', 2)	# base instruction for setting DRAM address
+    # set display data RAM (DDRAM) address for row/col
+    # (see pg. 196 of datasheet, noting 0x40 = b1000000 = 1 << 6)
+    write(ddram | ((row << 6) + col))
+
 
 def writeMessage(message):  # write a string to the display
-  for i in message:
-    write(ord(i), 1)
+    for i in message:
+        write(ord(i), 1)
+
 
 def clearDisplay():
-  write(int('00000001',2))	# clear display
+    write(int('00000001', 2))  # clear display
+
 
 def initialize():  # initialize the display:
-  write(int('00111000',2))	# Function set: 8-bit, 2 lines, 5x8 dots
-  write(int('00001100',2))	# Display ON, Cursor Off, Blinking Off
-  write(int('00000110',2))	# Entry Mode: Increment cursor, display shift Off
-  write(int('00000010',2))	# return home
-  write(int('00000001',2))	# clear display
+    write(int('00111000', 2))  # Function set: 8-bit, 2 lines, 5x8 dots
+    write(int('00001100', 2))  # Display ON, Cursor Off, Blinking Off
+    write(int('00000110', 2))  # Entry Mode: Increment cursor, display shift Off
+    write(int('00000010', 2))  # return home
+    write(int('00000001', 2))  # clear display
+
 
 def shift(steps, display = 0):     # shift cursor/display by # steps left/right
-  pass  # this is a lab 3 problem
+    pass  # this is a lab 3 problem
+
 
 def scroll(steps):  # scroll the screen left a set # of steps, then return
-  pass  # this is a lab 3 problem
+    pass  # this is a lab 3 problem
 
 initialize()
 
@@ -87,18 +97,17 @@ time.sleep(5)
 clearDisplay()
 url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
 theJSON = getJSON(url)
-set_row_col(0) 
+set_row_col(0)
 writeMessage(theJSON["metadata"]["title"])
-set_row_col(1) 
+set_row_col(1)
 writeMessage(str(theJSON["metadata"]["count"]) + " events recorded")
 time.sleep(3)
 while True:
-  for i in theJSON["features"]: # print events greater than 4
-    if i["properties"]["mag"] >= 4.0:
-      clearDisplay()
-      set_row_col(0)
-      writeMessage(i["properties"]["place"])
-      set_row_col(1)
-      writeMessage("Magnitude " + str(i["properties"]["mag"]))
-      time.sleep(3)
-
+    for i in theJSON["features"]:  # print events greater than 4
+        if i["properties"]["mag"] >= 4.0:
+            clearDisplay()
+            set_row_col(0)
+            writeMessage(i["properties"]["place"])
+            set_row_col(1)
+            writeMessage("Magnitude " + str(i["properties"]["mag"]))
+            time.sleep(3)
