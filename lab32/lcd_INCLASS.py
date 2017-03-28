ee07a890a2729041ccdfd9f11fc256df8f3d183d
nathan <njyoung94@gmail.com>
2017-03-27 21:34:08 -0400

    adjusted initialization, turning cursur on and blicking

--- lab32/lcd_INCLASS.py
+++ lab32/lcd_INCLASS.py
@@ -121,7 +121,7 @@ try:
     time.sleep(3)
     write(int('00010111', 2))
     shift(4)
-    #write(0b01000010, 1)  # write A
+    write(0b01000010, 1)  # write A
     time.sleep(3)
     write(int('00010011', 2))
     write(int('00010111', 2))
