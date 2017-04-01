66437720bc973e1e1c1673966f8f3ddb4cc291da
nathan <njyoung94@gmail.com>
2017-04-01 01:35:53 -0400

    started ws

--- WeatherStation/ws.py
+++ WeatherStation/ws.py
@@ -1,1 +1,8 @@
 # empty file to start things
+import Adafruit_BMP.BMP085 as BMP085
+
+sensor = BMP085.BMP085()
+
+print 'Temp = {0:0.1f} *C'.format(sensor.read_temperature())
+print 'Pressure = {0:0.1f} Pa'.format(sensor.read_pressure())
+print 'Altitude = {0:0.1f} m'.format(sensor.read_altitude())
