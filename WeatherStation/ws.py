893b2360b475d4cf59bb68de621b32670dfb3b47
nathan <njyoung94@gmail.com>
2017-04-01 21:31:18 -0400

    wrote example for hall effect

--- WeatherStation/ws.py
+++ WeatherStation/ws.py
@@ -1,19 +1,42 @@
 # empty file to start things
 
-# bmp example
-import Adafruit_BMP.BMP085 as BMP085
+# # bmp example
+# import Adafruit_BMP.BMP085 as BMP085
+#
+# sensor = BMP085.BMP085()
+#
+# print 'Temp = {0:0.1f} *C'.format(sensor.read_temperature())
+# print 'Pressure = {0:0.1f} Pa'.format(sensor.read_pressure())
+# print 'Altitude = {0:0.1f} m'.format(sensor.read_altitude())
+#
+#
+# #dht example slow refresh, only 1 hz
+# import Adafruit_DHT
+# import time
+#
+# sensor2 = Adafruit_DHT.DHT11
+# dataport = 4 # whatever gpio port it is on
+# print 'T = {0:0.1f}oC H = {1:0.1f}%'.format(T, H)
 
-sensor = BMP085.BMP085()
-
-print 'Temp = {0:0.1f} *C'.format(sensor.read_temperature())
-print 'Pressure = {0:0.1f} Pa'.format(sensor.read_pressure())
-print 'Altitude = {0:0.1f} m'.format(sensor.read_altitude())
-
-
-#dht example slow refresh, only 1 hz
-import Adafruit_DHT
+import RPi.GPIO as GPIO
 import time
 
-sensor2 = Adafruit_DHT.DHT11
-dataport = 4 # whatever gpio port it is on
-print 'T = {0:0.1f}oC H = {1:0.1f}%'.format(T, H)
+GPIO.setmode(GPIO.BCM)
+
+hall = 17
+
+GPIO.setup(hall, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.add_event_detect(4, GPIO.RISING)
+
+
+def my_callback():
+    print 'There it goes!'
+GPIO.add_event_callback(4, my_callback)
+
+try:
+    while True:
+        pass
+except:
+    print("Exiting...")
+    gpio.cleanup()
