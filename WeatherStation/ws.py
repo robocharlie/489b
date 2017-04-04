1886d602f58a0d6f48677a7e9e9c0244093d8a63
Nathan <njyoung94@gmail.com>
2017-04-04 09:52:56 -0400

    fixed 3008 error

--- WeatherStation/ws.py
+++ WeatherStation/ws.py
@@ -34,7 +34,7 @@ GPIO.setup(led, GPIO.OUT)
 # SPI setup
 SPI_PORT = 0
 SPI_DEVICE = 0
-mcp = Adafruit_MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 # DHT setup
 dht = Adafruit_DHT.DHT11
