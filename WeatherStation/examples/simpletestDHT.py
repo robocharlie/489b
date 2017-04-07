fffeb29b34fa75b7f1346a128dbe6e334a7ff37f
Nathan <njyoung94@gmail.com>
2017-04-07 15:18:32 -0400

    humidity and hall

--- WeatherStation/examples/simpletestDHT.py
+++ WeatherStation/examples/simpletestDHT.py
@@ -32,7 +32,7 @@ sensor = Adafruit_DHT.DHT11
 
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO23.
-pin = 23
+pin = 18
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
