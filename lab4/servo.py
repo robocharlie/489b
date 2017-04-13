9c988dc04de68ab6e3b9f041c6d2db2fcd82d75f
Nathan <njyoung94@gmail.com>
2017-04-13 10:00:07 -0400

    lab 4, servo.py

---
+++ lab4/servo.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+pwm_pin = 24
+GPIO.setup(pwm_pin, GPIO.OUT)
+
+min = 3
+max = 12
+
+pwm = GPIO.PWM(pwm_pin, 50)
+pwm.start(0)
+
+while(1):
+    for duty_cycle in range(min, max):
+        pwm.ChangeDutyCycle(duty_cycle)
+        print duty_cycle
+        time.sleep(.5)
