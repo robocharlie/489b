c130bb33bc8d264e55bb9c5988d425cff68d7b5f
Nathan <njyoung94@gmail.com>
2017-04-17 23:52:32 -0400

    lab 4, servo-process

---
+++ WeatherStation/pls/initial_test.py
@@ -0,0 +1,19 @@
+# import all the things
+
+
+
+# initalize the sensors and ports
+
+# timing variables
+
+# make a threshhold value for the photoresistors
+thresh = 800
+
+# from reading the sensor we get an array of ints
+
+rawval = [934, 354, 888, 266]
+
+# basically want to take these value, convert to a on/off and hav those numbers
+# represnt "bits" so we have 16 positions
+# use dictionary to define n/s/e/w
+
