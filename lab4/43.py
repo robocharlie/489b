e37c6dd6549b2dfe988d51888a82601cb296ace4
Nathan <njyoung94@gmail.com>
2017-04-21 18:38:44 -0400

    lab 4, 41

--- lab4/43.py
+++ lab4/43.py
@@ -26,7 +26,7 @@ class Pulser(multiprocessing.Process):
     def run(self):
         min_power = 1
         max_power = 100
-        fade_led = GPIO.PWM(self.pin, 1)
+        fade_led = GPIO.PWM(self.pin, min_power)
         fade_led.start(0)
         for duty_cycle in range(min_power, max_power + 1):
             fade_led.ChangeDutyCycle(duty_cycle)
