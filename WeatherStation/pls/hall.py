535ab95b6b503d3dad17c000994b133d3cfaac7a
Nathan <njyoung94@gmail.com>
2017-04-22 23:01:33 -0400

    Get directions for weathervane done

---
+++ WeatherStation/pls/hall.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+
+# threaded callback for when mag passes over
+# add time.time to list of values
+# have time intervals
+
+# a.append(time.time())
+# for val in a
+# if val < cutoff
+# a.remove(val)
